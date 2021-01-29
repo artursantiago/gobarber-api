6dd3172a49843b09f3aa3879d9633db83ba1763c
Artur Santiago <artursantiago@info.ufrn.br>
2021-01-28 21:06:58 -0300

    updating repository to typeORM

--- src/database/migrations/1611753346569-CreateAppointments.ts
+++ src/database/migrations/1611753346569-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1611753346569
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
