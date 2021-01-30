be5b760ed589ce6f07e123e1ab2edc27d668b544
Artur Santiago <artursantiago@info.ufrn.br>
2021-01-29 21:00:45 -0300

    create ManyToOne relation in Appointments

--- src/database/migrations/1611753346569-CreateAppointments.ts
+++ src/database/migrations/1611753346569-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1611753346569
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1611880261462-CreateUser.ts
+++ src/database/migrations/1611880261462-CreateUser.ts
@@ -8,7 +8,7 @@ export default class CreateUser1611880261462 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
